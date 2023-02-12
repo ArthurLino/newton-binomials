37927c1ceba6381b44833b7b27de34abdb3837ca
Arthur Lino <arthur.ssLino@gmail.com>
2023-02-12 11:22:21 -0300

    Fix: grammar errors

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ except:
     a, b, a_index = 1, 1, 1
     print("Not all input values are usable. Values were set to 1 automatically. ")
 
-multiplyer = 1
+multiplier = 1
 
 print(f"Then (a+b)^{a_index} =")
 result = (a+b)**a_index
 
 for loop_index, b_index in enumerate(range(a_index, -1, -1), start=1):
-    print(f"{loop_index}° nomial: +{multiplyer} * {a}^{b_index} * {b}^{a_index - b_index}")
-    multiplyer *= b_index / loop_index
-    multiplyer = math.ceil(multiplyer)
+    print(f"{loop_index}° nomial: +{multiplier} * {a}^{b_index} * {b}^{a_index - b_index}")
+    multiplier *= b_index / loop_index
+    multiplier = math.ceil(multiplier)
 
 print(f"Result: {result}")

75c7a4a4ade9605474ebb293f649b519e76d62ba
Arthur Lino <arthur.ssLino@gmail.com>
2023-03-25 11:58:51 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-""" (a+b)^x """
 import math
 
+super_s = [n for n in "⁰¹²³⁴⁵⁶⁷⁸⁹"]
+
 print("For (a+b)^x: ")
+
 try:
     a = int(input("Enter a: "))
     b = int(input("Enter b: "))
     a_index = int(input("Enter x: "))
 except:
-    a, b, a_index = 1, 1, 1
-    print("Not all input values are usable. Values were set to 1 automatically. ")
+    a, b, a_index = "a", "b", 5
+    print("Not all input values are usable. ")
 
-multiplier = 1
+multiplyer = 1
 
-print(f"Then (a+b)^{a_index} =")
-result = (a+b)**a_index
+print("Expresison: ")
 
 for loop_index, b_index in enumerate(range(a_index, -1, -1), start=1):
-    print(f"{loop_index}° nomial: +{multiplier} * {a}^{b_index} * {b}^{a_index - b_index}")
-    multiplier *= b_index / loop_index
-    multiplier = math.ceil(multiplier)
-
-print(f"Result: {result}")
+    if loop_index != 1:
+        print("+", end=" ")
+    if multiplyer != 1:
+        print('{}'.format(multiplyer), end='*')
+    if b_index > 0:
+        print('{}{}'.format(a ,''.join(super_s[int(l)] for l in str(b_index))), end='')
+    if a_index-b_index > 0 :
+        print('*{}{}'.format(b, ''.join(super_s[int(l)] for l in str(a_index-b_index))), end=' ')
+    multiplyer *= b_index / loop_index
+    multiplyer = math.ceil(multiplyer)
